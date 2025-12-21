51b58781073e76800475061ca31d6e04b91656a9
stanK-user <ginsoakedboyprod@proton.me>
2025-12-21 00:58:04 +0000

    Deploying to gh-pages from @ stanK-user/smyrnosworld.github.io@17222d13277883dd0a208ae6e39cc7409c2ba444 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "stanK-user/Smyrnos-World",
+    "data-repo": "stanK-user/smyrnosworld.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
